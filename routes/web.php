338e7c6f34244fbc73199514a511695be01cb107
groupmarketingmbn-hash <groupmarketing.mbn@gmail.com>
2026-06-12 15:23:11 +0700

    fix: add explicit Route::bind for content_calendar to ContentItem

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
+    Route::bind('content_calendar', fn($value) => \App\Models\ContentItem::findOrFail($value));
     Route::resource('content-calendar', ContentCalendarController::class);
 
     Route::get('/database', [DatabaseController::class, 'index'])->name('database.index');
